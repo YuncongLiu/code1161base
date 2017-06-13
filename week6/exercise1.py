bcb60217a01a85db13fae929e57c03d380129fa9
mika_yuncong Liu <mikaliuyuncong@gmail.com>
2017-06-13 23:14:27 +1000

    finished

--- week6/exercise1.py
+++ week6/exercise1.py
@@ -1,10 +1,9 @@
-"""
-Docstring
-"""
+"""Docstring."""
 from __future__ import division
 from __future__ import print_function
 
 
 def function_that_returns_something():
+    """Docstring."""
     print("week 6, ready for action!")
     return True
